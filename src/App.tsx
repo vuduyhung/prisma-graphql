1205eb7cc4e36025d6c41900d99add2909f5ec84
Hung Vu <vn.vuduyhung@gmail.com>
2024-05-27 16:30:01 +0700

    part 1: initialize frontend with mock data

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { User } from './types'
+import UserDisplay from './components/UserDisplay'
+
+function App() {
+  const users: User[] = [{
+    name: 'Prisma Fan',
+    messages: [{
+      body: 'Prisma rocks!!'
+    }, {
+      body: 'Did I mention I love Prisma?'
+    }]
+  }]
+
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      {
+        users.map((user, i) => <UserDisplay user={user} key={i} />)
+      }
+    </div>
+  )
+}
+
+export default App
