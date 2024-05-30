f05c6b588f82bb9f2c8cf2f630e817a189803f5b
Hung Vu <vn.vuduyhung@gmail.com>
2024-05-31 01:06:04 +0700

    add urql graphql client and update code to get data from server

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+//#region urql OLD VERSION
+// import { createClient, Provider } from 'urql';
+
+// const client = createClient({
+//   url: import.meta.env.VITE_API_URL || 'http://localhost:4000/graphql',
+//   exchanges: []
+// });
+//#endregion
+
+import { Client, Provider, cacheExchange, fetchExchange } from 'urql';
+
+const client = new Client({
+  url: import.meta.env.VITE_API_URL || 'http://localhost:4000/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider value={client}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
